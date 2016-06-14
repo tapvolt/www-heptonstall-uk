ce0357fe8f47db2343ae15967f59d154ec1911df
Gareth Jackson <gareth.jackson@gmail.com>
2016-06-14 21:22:00 +0100

    added step to remove node_modules directory

--- app/app.component.js
+++ app/app.component.js
@@ -1,8 +1,8 @@
 (function(app) {
     app.AppComponent =
         ng.core.Component({
-            selector: 'my-app',
-            template: '<h1>Heptonstall Weather Webcam</h1>'
+            selector: "my-app",
+            template: "<h1>Heptonstall Weather Webcam</h1>"
         })
         .Class({
             constructor: function() {}
