4551e2f96fe5f5a6a0d30fdf9f647944d35b86b4
Gareth Jackson <gareth.jackson@gmail.com>
2016-11-20 20:34:15 +0000

    new header component

--- www/js/source/app.js
+++ www/js/source/app.js
@@ -2,16 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Logo from './components/Logo';
+
+import Header from './components/Header';
 import Image from './components/Image';
 import Copy from './components/Copy';
 import Weather from './components/WeatherUnderground';
 
 ReactDOM.render(
     <div>
-        <h1>
-            <Logo /> Heptonstall Weather Webcam
-        </h1>
+
+        <Header />
 
         <Image />
 
