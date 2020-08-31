9858c7de1b815a69e24a1c2ca132226343e69045
Gareth <tapvolt@users.noreply.github.com>
2020-08-31 21:28:55 +0100

    css and layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './componets/App';
+import App from './componets/app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
